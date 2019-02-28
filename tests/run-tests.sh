c51e7c7af9f9afb33ef32eef261e8bfd5c1e2b81
Umesh Parulekar <ucparule@us.ibm.com>
2019-02-28 13:24:32 -0600

    oneibmcloud/DevOps-Insights#5126 added code to send build record

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,5 +4,7 @@ npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
 npm install
 npm install -g grunt-idra3
 
+idra --publishbuildrecord --branch=$GIT_BRANCH --repositoryurl=$GIT_URL --commitid=$GIT_COMMIT --status=pass
+
 idra --publishtestresult --filelocation=./tests/xunit.xml --type=unittest
 idra --publishtestresult --filelocation=./tests/Cobertura.xml --type=code
