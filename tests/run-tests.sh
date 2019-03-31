86a078c349219d8a4f0011f11f59a67e3de51bdc
Philippe Mulet <hmagph@users.noreply.github.com>
2019-03-31 07:01:18 +0000

    upgrade node version

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export PATH=/opt/IBM/node-v4.2/bin:$PATH
-npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
 npm install
 npm install -g grunt-idra3
 
